e24dccff243bc66174892f3f3ffae50f4e2310e8
Mathieu Grégoire <https://github.com/greg-mathieu>
2018-05-17 17:12:51 +0200

    ajout de profildans la firebase

---
+++ app/src/main/java/fr/nicolashoareau_toulousewcs/hack2starlover/NewProfilActivity.java
@@ -0,0 +1,154 @@
+package fr.nicolashoareau_toulousewcs.hack2starlover;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class NewProfilActivity extends AppCompatActivity {
+
+    String mSide;
+    ImageView mProfilPic;
+    EditText mPseudo;
+    private Uri mUri = null;
+
+    private DatabaseReference mDatabaseReference;
+    private FirebaseDatabase mDatabase;
+    private FirebaseAuth mAuth;
+    private StorageReference mStorageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_profil);
+
+        mSide = getIntent().getExtras().getString("Extra");
+        setTitle("You choose the " + mSide + " Side !");
+
+        mProfilPic = findViewById(R.id.iv_profil_pic);
+        mPseudo = findViewById(R.id.et_pseudo);
+        Button validation = findViewById(R.id.button_validation);
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mStorageReference = FirebaseStorage.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        mProfilPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, 1);
+            }
+        });
+
+        validation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = mPseudo.getText().toString();
+                if (firstName.isEmpty()) {
+                    Toast.makeText(NewProfilActivity.this, "Choisis ton nom !", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    saveUserModel();
+
+                    Intent intent = new Intent(NewProfilActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 1:
+                if(resultCode == RESULT_OK){
+                    mUri = imageReturnedIntent.getData();
+                    Glide.with(NewProfilActivity.this).load(mUri)
+                            .apply(RequestOptions.circleCropTransform()).into(mProfilPic);
+
+                }
+                break;
+        }
+    }
+
+    private void saveUserModel() {
+        final String pseudo = mPseudo.getText().toString();
+
+        /*if (mUri == null){
+
+            if (mLink != null){
+                String profilPic = mLink;
+                UserModel userModel = new UserModel(pseudo, profilPic);
+                FirebaseUser user = mAuth.getCurrentUser();
+                mDatabaseReference = mDatabase.getReference("User");
+                mDatabaseReference.child(user.getUid()).child("Profil").setValue(userModel);
+            }
+            else{
+                String profilPic = mUrlSave;
+                UserModel userModel = new UserModel(pseudo, profilPic);
+                FirebaseUser user = mAuth.getCurrentUser();
+                mDatabaseReference = mDatabase.getReference("User");
+                mDatabaseReference.child(user.getUid()).child("Profil").setValue(userModel);
+            }
+
+
+        }
+        else {
+            StorageReference filePath = mStorageReference.child("profilPicture").child(mUri.getLastPathSegment());
+            filePath.putFile(mUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    String profilPic = downloadUrl.toString();
+                    UserModel userModel = new UserModel(pseudo, profilPic);
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    mDatabaseReference = mDatabase.getReference("User");
+                    mDatabaseReference.child(user.getUid()).child("Profil").setValue(userModel);
+                }
+            });
+        }*/
+
+        StorageReference filePath = mStorageReference.child("profilPicture").child(mUri.getLastPathSegment());
+        filePath.putFile(mUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                String profilPic = downloadUrl.toString();
+                UserModel userModel = new UserModel(pseudo, profilPic);
+                FirebaseUser user = mAuth.getCurrentUser();
+                mDatabaseReference = mDatabase.getReference("User");
+                mDatabaseReference.child(mSide).child(user.getUid()).child("Profil").setValue(userModel);
+            }
+        });
+
+
+
+    }
+}
