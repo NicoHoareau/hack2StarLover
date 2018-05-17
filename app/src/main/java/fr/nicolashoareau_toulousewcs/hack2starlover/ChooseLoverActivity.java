10b540f131b770ec077491b6643d8f54cca87166
Coralie Delplace <coralie.delplace@gmail.com>
2018-05-17 13:28:46 +0200

    GridViewVide

--- app/src/main/java/fr/nicolashoareau_toulousewcs/hack2starlover/ChooseLoverActivity.java
+++ app/src/main/java/fr/nicolashoareau_toulousewcs/hack2starlover/ChooseLoverActivity.java
@@ -2,15 +2,21 @@ package fr.nicolashoareau_toulousewcs.hack2starlover;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class ChooseLoverActivity extends AppCompatActivity {
+    private GridView mLoverGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_lover);
 
+        mLoverGrid = findViewById(R.id.grid_lovers);
 
 
     }
 }
+
