f9ab8839dff4b826289c5bc49d1e97c187d946cc
NicoHoareau <nicolas.hoareau.pro@orange.fr>
2018-05-17 11:02:06 +0200

    add choose-lover xml et activity noactionbar

---
+++ app/src/main/java/fr/nicolashoareau_toulousewcs/hack2starlover/ChooseLoverActivity.java
@@ -0,0 +1,15 @@
+package fr.nicolashoareau_toulousewcs.hack2starlover;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseLoverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_lover);
+
+
+    }
+}
